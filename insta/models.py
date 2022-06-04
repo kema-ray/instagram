9fc3418abfdeace1052e8da55f6d11086fb2b488
rachel <rachelkemuma99@gmail.com>
2022-06-04 18:38:36 +0300

    create class Post and do migrations

--- insta/models.py
+++ insta/models.py
@@ -1,3 +1,16 @@
+from distutils.command.upload import upload
+from email.mime import image
+from time import timezone
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    image = models.ImageField(upload_to='uploads/')
+    title = models.CharField(max_length=100,blank=True)
+    content = models.TextField()
+    # date_posted = models.DateTimeField(default=timezone.now)
+    likes = models.IntegerField(null=True, default=0)
+
+    def __str__(self):
+        return self.title
+
