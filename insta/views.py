f4a87bd575eb6644663189a6d897335445af984f
rachel <rachelkemuma99@gmail.com>
2022-06-05 16:24:12 +0300

    create forms to update user and profile

--- insta/views.py
+++ insta/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import *
 from django.contrib.auth.decorators import login_required
+from .forms import *
 
 # Create your views here.
 @login_required(login_url='/accounts/login/')
