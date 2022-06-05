b28256c6fa02184a6613ed813e230945b49d43ad
rachel <rachelkemuma99@gmail.com>
2022-06-05 06:24:45 +0300

    Create fields in the models

--- insta/models.py
+++ insta/models.py
@@ -1,8 +1,5 @@
-from distutils.command.upload import upload
-from email.mime import image
-from time import timezone
-from unicodedata import name
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Post(models.Model):
@@ -11,14 +8,16 @@ class Post(models.Model):
     content = models.TextField()
     # date_posted = models.DateTimeField(default=timezone.now)
     likes = models.IntegerField(null=True, default=0)
+    author = models.ForeignKey(User,on_delete=models.CASCADE,default='',null='True',related_name='author')
 
     def __str__(self):
         return self.title
 
 class Profile(models.Model):
     name = models.CharField(max_length=30)
-    pic = models.ImageField(upload_to='uploads')
+    pic = models.ImageField(upload_to='uploads/')
     bio = models.TextField()
+    user = models.OneToOneField(User,on_delete=models.CASCADE,default='',null=True)
 
     # def __str__(self) -> str:
     #     return f'{self.user} Profile'
