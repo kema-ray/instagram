e246674663e021395461d8a7249f2176f2996277
rachel <rachelkemuma99@gmail.com>
2022-06-07 08:44:55 +0300

    add tests

--- insta/models.py
+++ insta/models.py
@@ -1,4 +1,4 @@
-from pyexpat import model
+
 from django.db import models
 from django.dispatch import receiver
 from django.utils import timezone
@@ -64,5 +64,10 @@ class Comment(models.Model):
     user = models.ForeignKey(Profile,on_delete=models.CASCADE,related_name="comments")
     created = models.DateTimeField(auto_now_add=True,null=True)
 
+    @classmethod
+    def delete_comment(cls,id):
+        comment = Comment.objects.get(id=id)
+        comment.delete()
+        
     def __str__(self):
         return f'{self.user.name} Post'
--- insta/tests.py
+++ insta/tests.py
@@ -1,6 +1,60 @@
-
+from unicodedata import name
 from django.test import TestCase
 from .models import *
 from django.contrib.auth.models import User
 
 # Create your tests here.
+class TestUser(TestCase):
+    def setUp(self):
+        self.rachel = User(username='ray',email='test@gmail.com',password='test123')
+        self.rachel.save()
+
+    def test_user_instance(self):
+        self.assertTrue(isinstance(self.rachel,User))
+
+class TestProfile(TestCase):
+    def setUp(self):
+        self.rachel = User(username='ray',email='test@gmail.com',password='test123')
+        self.rachel.save()
+
+        self.e_profile = Profile(user=self.rachel,pic = "../uploads/img.png",name="ray",bio="hello world")
+
+    def test_profile_instance(self):
+        self.assertTrue(isinstance(self.e_profile,Profile))
+class TestPost(TestCase):
+    def setUp(self):
+        self.e_profile = Profile(pic = "../uploads/img.png",name="ray",bio="hello world")
+        self.e_profile.save()
+
+        self.e_post = Post(image = "../uploads/img.png",title = "hello world",content="first program",likes = 10,date_posted= "June 5, 2022, 12:19 p.m.")
+
+    def tearDown(self):
+        Profile.objects.all().delete()
+        Post.objects.all().delete()
+    
+    def test_post_instance(self):
+        self.assertTrue(isinstance(self.e_post,Post))
+
+class TestComment(TestCase):
+    def setUp(self):
+        self.rachel = User(username='ray',email='test@gmail.com',password='test123')
+        self.rachel.save()
+
+        self.e_profile = Profile(pic = "../uploads/img.png",name="ray",bio="hello world")
+        self.e_profile.save()
+
+        self.e_post = Post(image = "../uploads/img.png",title = "hello world",content="first program",likes = 10,date_posted= "2022-06-04 12:19")
+        self.e_post.save()
+
+        self.e_comment = Comment(comment =self.e_profile, created ="2022-06-04 12:19",post = self.e_post)
+
+    def tearDown(self):
+        Profile.objects.all().delete()
+        Post.objects.all().delete()
+        User.objects.all().delete()
+    
+    def test_comment_instance(self):
+        self.assertTrue(isinstance(self.e_comment, Comment))
+
+
+
